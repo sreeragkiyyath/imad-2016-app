2724bfd87963f1026ce4e9ea32ba4c491afcab6a
sreeragkiyyath <csreeragk@gmail.com>
2016-10-23 23:42:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,24 @@ console.log('Loaded!');
 
 var button = document.getElementById('button-counter');
 button.onclick = function(){
-var request = new XMLHttpRequest();		
+            var request = new XMLHttpRequest();		
 			request.onreadystatechange = function () {
 				if(request.readyState === XMLHttpRequest.DONE){
 					if(request.status ===200) {
-						var counter = request.responseText;
+				/*		var counter = request.responseText;
 						var span = document.getElementById('count');
-						span.innerHTML = counter.toString();
+						span.innerHTML = counter.toString();*/
+						
+						var names = ['name1','name2','name3','name4'];
+                          var list = '';
+                          for(var i = 0; i < names.length; i++) {
+                                  list+='<li>'+names[i]+'</li>';
+        
+                             }
+                               var ul =  document.getElementById('nameList');
+                             ul.innerHtml = list; 
+						
+						
 					} 
 				}
 			};
@@ -23,12 +34,32 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
     
-    var names = ['name1','name2','name3','name4'];
+   /* var names = ['name1','name2','name3','name4'];
     var list = '';
     for(var i = 0; i < names.length; i++) {
         list+='<li>'+names[i]+'</li>';
         
     }
     var ul =  document.getElementById('nameList');
-    ul.innerHtml = list;    
+    ul.innerHtml = list;    */
+    
+    
+      var request = new XMLHttpRequest();		
+	  request.onreadystatechange = function () {
+				if(request.readyState === XMLHttpRequest.DONE){
+					if(request.status ===200) {
+					    var names = ['name1','name2','name3','name4'];
+                        var list = '';
+                          for(var i = 0; i < names.length; i++) {
+                                  list+='<li>'+names[i]+'</li>';
+        
+                        }
+                         var ul =  document.getElementById('nameList');
+                         ul.innerHtml = list; 
+						
+						
+					} 
+				}
 };
+		request.open('GET','http://https://cloud.imad.hasura.io:8080/submit-name?name='+name,true);
+		request.send(null);
