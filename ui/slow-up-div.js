123c7942164330b246018f698ba4afecfd1d57fe
SREERAG K <csreeragk@gmail.com>
2016-10-31 23:45:18 +0530

    [imad-console] Updates ui/slow-up-div.js

--- ui/slow-up-div.js
+++ ui/slow-up-div.js
@@ -18,6 +18,14 @@
 
 
 $(document).ready(function() {
+    
+    
+    
+    
+    
+    
+    
+    
 $("#product").click(function() {
     $('html, body').animate({scrollTop: $(".gallery-set").offset().top }, 1700);
 });
@@ -58,6 +66,53 @@ submit.onclick=function(){
     
     
 };
+
+
+
+
+var submitt = document.getElementById('check');
+submitt.onclick=function(){
+    alert("helloo");
+      var request = new XMLHttpRequest();		
+	  request.onreadystatechange = function () {
+				if(request.readyState === XMLHttpRequest.DONE){
+					if(request.status ===200) {
+					    var names = request.responseText;
+					    names = JSON.parse(names);
+                     
+                        var list = '';
+                          for(var i = 0; i < names.length; i++) {
+                                  list+='<li>'+names[i]+'</li>';
+        
+                        }
+                         var ul =  document.getElementById('nameList');
+                         ul.innerHTML = list; 
+					} 
+				}
+};
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+		request.open('GET','http://https://cloud.imad.hasura.io:8080/submit-name?name='+name,true);
+		request.send(null);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
